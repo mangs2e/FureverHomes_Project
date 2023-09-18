a6d30e856d700228b7abf339203113ef4ed0c2be
Myeongjin <mangs2e@naver.com>
2023-09-18 15:58:16 +0900

    초기화

---
+++ src/main/java/com/example/fureverhomes_project/FureverHomesProjectApplication.java
@@ -0,0 +1,14 @@
+package com.example.fureverhomes_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@SpringBootApplication
+public class FureverHomesProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FureverHomesProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/fureverhomes_project/FureverHomesProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fureverhomes_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FureverHomesProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
