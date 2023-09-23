ff56c1febc4d0e6b4f5ddcb029f8df292457840f
Myeongjin <mangs2e@naver.com>
2023-09-23 18:33:37 +0900

    Animal 엔티티를 관리할 레파지토리 생성

--- src/main/java/com/example/fureverhomes_project/repository/AnimalRepository.java
+++ src/main/java/com/example/fureverhomes_project/repository/AnimalRepository.java
@@ -2,8 +2,11 @@ package com.example.fureverhomes_project.repository;
 
 import com.example.fureverhomes_project.entity.Animal;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+//동물 관련 작업을 위한 레파지토리
+@Repository
 public interface AnimalRepository extends JpaRepository<Animal, Long> {
 }
